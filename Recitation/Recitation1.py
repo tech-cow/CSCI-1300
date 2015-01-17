cf00ce070f796c173545825e8e4185bb058573d6
Yu Zhou <junior147147@yahoo.com>
2015-01-16 22:49:57 -0700

    Update Recitation1.py

--- Recitation/Recitation1.py
+++ Recitation/Recitation1.py
@@ -1,9 +1,11 @@
 Sum in Python
---------------------
+-------------------
 
 ```python
+
 x = input("Enter the first number ")
 y = input("Enter the second number ")
 sum = x + y
 print "the sum is",  sum
+
 ```
