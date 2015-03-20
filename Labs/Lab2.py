e24c75c913875375b8577318b03973edebbf504d
Yu Zhou <junior147147@yahoo.com>
2015-03-20 15:52:49 -0600

    Basic Calculations

--- Labs/Lab2.py
+++ Labs/Lab2.py
@@ -6,4 +6,4 @@ sales_tax = .13
 num_items = 2
 total_price = 1999.915
 cost_per_item = (total_price - sales_tax*num_items) / num_items
-print "The cost of each 2 item is $%.2f" % cost_per_item
+print "The cost of each 2 item is $%.2f" % cost_per_item 
