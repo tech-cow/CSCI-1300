771b02bc77941b08f9ab4bb194552a318d1d845e
Yu Zhou <junior147147@yahoo.com>
2015-05-06 14:15:14 -0600

    C++ Intro

---
+++ Labs/Lab9/Yu_Zhou_Lab9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+string name;
+
+int main()
+{
+	cout << "Enter your name!" << endl;
+	cin >> name;
+	cout << "Hello " << name << "!" <<endl;
+
+
+	return 0;
+}
